450ac2970c1b9e47f54af62557dcacaa9ab591a6
zzz <1475599306@qq.com>
2023-12-26 22:32:35 +0800

    手写ArrayList

--- day02_list/src/main/java/com/weisong/Test01.java
+++ day02_list/src/main/java/com/weisong/Test01.java
@@ -2,19 +2,9 @@ package com.weisong;
 
 public class Test01 {
     public static void main(String[] args) {
-      //冒泡排序
-        int[] arr = {1, 3, 2, 5, 4, 7, 6, 9, 8};
-        for (int i = 0; i < arr.length - 1; i++) {
-          for (int j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-              int temp = arr[j];
-              arr[j] = arr[j + 1];
-              arr[j + 1] = temp;
-            }
-          }
-        }
-        for (int i : arr) {
-            System.out.println(arr[i]);
-        }
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.remove("1");
+        System.out.println(list);
     }
 }
