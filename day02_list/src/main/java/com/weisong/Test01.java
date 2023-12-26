fbadb0d52f54feb25f8a050c0d464877b167866c
zzz <1475599306@qq.com>
2023-12-26 22:14:18 +0800

    算法02-链表

---
+++ day02_list/src/main/java/com/weisong/Test01.java
@@ -0,0 +1,20 @@
+package com.weisong;
+
+public class Test01 {
+    public static void main(String[] args) {
+      //冒泡排序
+        int[] arr = {1, 3, 2, 5, 4, 7, 6, 9, 8};
+        for (int i = 0; i < arr.length - 1; i++) {
+          for (int j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+              int temp = arr[j];
+              arr[j] = arr[j + 1];
+              arr[j + 1] = temp;
+            }
+          }
+        }
+        for (int i : arr) {
+            System.out.println(arr[i]);
+        }
+    }
+}
