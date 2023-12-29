48b7e5730e45f15d3768d8eb07f2a9b9d42f6bfd
zzz <1475599306@qq.com>
2023-12-29 22:24:40 +0800

    链表

--- day03_linked/src/main/java/com/weisong/LinkedList.java
+++ day03_linked/src/main/java/com/weisong/LinkedList.java
@@ -1,17 +1,138 @@
 package com.weisong;
 
-public class LinkedList {
-    private int size;
-    private Node firstNode;
-    private static class Node<E>{
-        E element;
-        Node<E> nextNode;
 
-        public Node(E element, Node<E> nextNode) {
+public class LinkedList<E> extends AbstractList<E> {
+    private Node<E> first;
+    private Node<E> last;
+
+    private static class Node<E> {
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
             this.element = element;
-            this.nextNode = nextNode;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            if (prev != null) {
+                sb.append(prev.element);
+            } else {
+                sb.append("null");
+            }
+
+            sb.append("_").append(element).append("_");
+
+            if (next != null) {
+                sb.append(next.element);
+            } else {
+                sb.append("null");
+            }
+
+            return sb.toString();
         }
     }
 
+    @Override
+    public void clear() {
+        size = 0;
+        first = null;
+    }
 
+    @Override
+    public E get(int index) {
+        return node(index).element;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index == 0) {
+            first = new Node<>(null, element, first);
+        } else {
+            Node<E> node = node(index - 1);
+            Node<E> prev = node.prev;
+            prev.next = new Node(prev, element, prev.next);
+        }
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        Node<E> node = first;
+        if (index == 0) {
+            first = first.next;
+            first.prev = null;
+        } else {
+            Node<E> prev = node(index - 1);
+            node = prev.next;
+            prev.next = prev.next.next;
+            prev.next.next.prev = prev;
+        }
+        size--;
+        return node.element;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        if (element == null){
+            Node<E> node = first;
+            for(int i =0; i<size; i++){
+                if(node.element == null) return i;
+                node = node.next;
+            }
+        }else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if(node.element.equals(element))return i;
+                node = node.next;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 获取index节点位置对象
+     *
+     * @param index
+     * @return
+     */
+    private Node<E> node(int index) {
+        rangeCheck(index);
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("size=").append(size).append(", [");
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                string.append(", ");
+            }
+
+            string.append(node);
+
+            node = node.next;
+        }
+        string.append("]");
+        return string.toString();
+    }
 }
