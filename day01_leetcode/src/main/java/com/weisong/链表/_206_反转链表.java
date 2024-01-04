b95d0aa31ff1a76062530a305bb6ae0116719eb9
zzz <1475599306@qq.com>
2024-01-04 19:47:24 +0800

    链表

---
+++ day01_leetcode/src/main/java/com/weisong/链表/_206_反转链表.java
@@ -0,0 +1,9 @@
+package com.weisong.链表;
+
+public class _206_反转链表 {
+
+    public ListNode reverseList(ListNode head) {
+
+        return null;
+    }
+}
---
+++ day01_leetcode/src/main/java/com/weisong/链表/_237_删除链表中的节点.java
@@ -0,0 +1,12 @@
+package com.weisong.链表;
+
+/**
+ * https://leetcode.cn/problems/delete-node-in-a-linked-list/
+ */
+public class _237_删除链表中的节点 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
