3bc63cf987312ff77e452ccc633d7b7d00ac97d3
zzz <1475599306@qq.com>
2024-01-07 21:06:04 +0800

    反转链表

--- day01_leetcode/src/main/java/com/weisong/链表/_206_反转链表.java
+++ day01_leetcode/src/main/java/com/weisong/链表/_206_反转链表.java
@@ -3,7 +3,14 @@ package com.weisong.链表;
 public class _206_反转链表 {
 
     public ListNode reverseList(ListNode head) {
-
-        return null;
+        if (head == null || head.next == null) return head;
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
     }
 }
